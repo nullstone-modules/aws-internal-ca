aa4a3f183e1dd80201c490a474bd09a5b7738d25
Brad Sickles <brad.sickles@gmail.com>
2021-10-15 12:36:56 -0400

    Added root_subject to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
 output "root_cert_pem" {
-  value = tls_self_signed_cert.this.cert_pem
+  value       = tls_self_signed_cert.this.cert_pem
+  description = "string ||| The root certificate pem"
 }
 
 output "root_key_secret_id" {
-  value = aws_secretsmanager_secret.private_key.id
+  value       = aws_secretsmanager_secret.private_key.id
+  description = "string ||| The AWS secrets manager secret id containing the root private key pem"
+}
+
+output "root_subject" {
+  value       = tls_self_signed_cert.this.subject
+  description = "object({ common_name: string, organization: string }) ||| The subject on the root certificate"
 }
