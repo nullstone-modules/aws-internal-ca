f1f6f2968a3bb8a637127d41c50a957ed056db41
Brad Sickles <brad.sickles@gmail.com>
2022-07-26 22:02:14 -0400

    Fix deprecation of key_algorithm

--- root.tf
+++ root.tf
@@ -4,10 +4,9 @@ resource "tls_private_key" "this" {
 }
 
 resource "tls_self_signed_cert" "this" {
-  key_algorithm         = "RSA"
   private_key_pem       = tls_private_key.this.private_key_pem
   validity_period_hours = var.valid_hours
-  is_ca_certificate     = true
+  is_ca_certificate      = true
 
   subject {
     common_name  = var.common_name
